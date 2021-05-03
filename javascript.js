9d7949e2e018bea97ff8ba87dff5066f378c70c6
ReeseAnderson0 <75329275+ReeseAnderson0@users.noreply.github.com>
2021-05-03 00:10:11 -0400

    initialcommit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+VANTA.CELLS({
+    el: "#vanta-canvas",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: true,
+    minHeight: 500.00,
+    minWidth: 5999.00,
+    scale: 1.00,
+    color1: 09553,
+    color2: 0xffffff,
+    size: 3.30,
+    speed: 1.10
+})
