8e26b977fe206bf43280ce09aea9caacae9cdf52
Reese <75329275+ReeseAnderson0@users.noreply.github.com>
2021-11-26 16:57:16 -0500

    Added School Dir

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var port = process.env.Port || 8080
+
+app.use(express.static(__dirname));
+
+app.use(bodyParser.json());
+
+// routes
+app.post("/Doge", (dogeReq, response) => {
+    console.log('you posted: \n');
+    console.log(dogeReq.body);
+    const fs = require('fs');
+    let data = JSON.stringify(dogeReq.body);
+    fs.writeFileSync('School/dogeJSON.json', data);
+});
+
+app.get("/", function(req, res) {
+    res.render("index")
+})
+
+app.listen(port, function() {
+    console.log("Online");
+})
