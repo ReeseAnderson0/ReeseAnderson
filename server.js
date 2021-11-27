0877c2e8c12be1f92154185d5c6899b5fc6ecb3e
Reese <75329275+ReeseAnderson0@users.noreply.github.com>
2021-11-27 13:14:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+
 var app = express();
 
 const server = app.listen(process.env.PORT || 5000, () => {
     const port = server.address().port;
-    console.log(`Online listening at port: ${port}`);
+    console.log(`Express is working on port ${port}`);
 });
 
-
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname));
 
 app.use(bodyParser.json());
 
 // routes
-app.post("", (dogeReq, response) => {
+app.post("/Doge", (dogeReq, response) => {
     console.log('you posted: \n');
     console.log(dogeReq.body);
     const fs = require('fs');
@@ -24,4 +24,3 @@ app.post("", (dogeReq, response) => {
 app.get("/", function(req, res) {
     res.render("index")
 })
-
